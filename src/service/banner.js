a14564734ba91a674ba488feb0e9f8837422fdfc
francisw1989 <838883685@qq.com>
2019-09-03 09:39:55 +0800

    32

--- src/service/banner.js
+++ src/service/banner.js
@@ -1,5 +1,8 @@
+import axios from 'axios';
+const u = 'https://zzh.hzysofti.com:8002/api/v1/';
 let bannerService = {
     getBannerList(){
+
         let p = new Promise((resolve, reject)=>{
             let data = [
                 {a: '232131',b:'但是',c:'水电费',d:'地方',e:'df'},
