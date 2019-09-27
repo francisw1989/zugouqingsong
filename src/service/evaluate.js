a52a11250e483625adad9b3d529f5a47448a2e76
francisw1989 <838883685@qq.com>
2019-09-27 11:12:55 +0800

    32

--- src/service/evaluate.js
+++ src/service/evaluate.js
@@ -17,6 +17,21 @@ let evaluateService = {
         return p;
     },
 
-    
+    // /api/v1/evaluationLabel/list
+    // 所有标签列表查询
+    getLabList(params){
+        let p = new Promise((resolve, reject)=>{
+            let data = {
+                method: 'get',
+                url: '/api/v1/evaluationLabel/list',
+                params: params,
+                loading: true,
+            }
+            $axios(data).then((res)=>{
+                resolve(res)
+            })
+        })
+        return p;
+    },
 }
 export { evaluateService }
--- src/view/va/evaluate/evaluateTag.vue
+++ src/view/va/evaluate/evaluateTag.vue
@@ -10,30 +10,27 @@
             <el-table :data="list"  border class="table" ref="multipleTable">
                 <!-- <el-table-column type="selection" width="55" align="center"></el-table-column> -->
                 <!-- <el-table-column type="index" label="序号"  width="50" align='center'></el-table-column> -->
-                <el-table-column prop="a" label="评价" width="150"></el-table-column>
-                <el-table-column prop="b" label="对应分值" width="120"></el-table-column>
-                <el-table-column prop="c" label="标签"></el-table-column>
+                <el-table-column prop="evaluateLevel" label="评价" width="150"></el-table-column>
+                <el-table-column prop="evaluateScore" label="对应分值" width="120"></el-table-column>
+                <el-table-column prop="evaluateLabel" label="标签"></el-table-column>
                 <el-table-column label="操作" width="150" align="center">
                     <template slot-scope="scope">
                         <el-button size="mini" @click="handle1(scope.$index, scope.row)">编辑</el-button>
                     </template>
                 </el-table-column>
             </el-table>
-            <div class="pagination">
-                <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="1000">
-                </el-pagination>
-            </div>
+
         </div>
         <el-dialog :title="idx==-1?'新增优惠券':'修改优惠券'" :visible.sync="editVisible" width="500px">
             <el-form ref="form" :model="form" :rules="rules" label-width="120px">
-                <el-form-item label="评价分类" prop="a">
+                <el-form-item label="评价名称" prop="a">
                     <el-select v-model="form.pjfl" placeholder="">
                         <el-option v-for="item in pjflList" :key="item" :label="item" :value="item"></el-option>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="分值" prop="a">
+                <!-- <el-form-item label="分值" prop="a">
                     <el-input v-model="form.a" style="width: 80px" placeholder="分"></el-input>
-                </el-form-item>
+                </el-form-item> -->
                 <el-form-item label="评价标签">
                     <el-tag :key="tag" v-for="tag in form.tags" closable :disable-transitions="false" @close="handleClose(tag)" class="right5">
                     {{tag}}
@@ -60,7 +57,12 @@
 <script>
     import bus from '../../../bus';
     import {evaluateService} from '../../../service/evaluate';
-
+    const Form = {
+        id: '',
+        evaluateScore: '',
+        evaluateLevel: '',
+        evaluateLabel: ''
+    }
     export default {
         data() {
             return {
@@ -93,6 +95,13 @@
             
         },
         methods:{
+            getlist(){
+                const t = this;
+                evaluateService.getLabList({}).then((res)=>{
+                    t.list = res;
+                });
+
+            },
             handleClose(tag) {
                 this.form.tags.splice(this.form.tags.indexOf(tag), 1);
             },
@@ -167,9 +176,7 @@
         },
         mounted(){
             const t = this;
-            evaluateService.getEvaluateList().then((res)=>{
-                t.list = res;
-            });
+            t.getlist();
             t.xmflList = t.$GD.xmflList;
             t.pjflList = t.$GD.pjflList;
 
