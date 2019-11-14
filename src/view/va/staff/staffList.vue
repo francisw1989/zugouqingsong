2e1bdf4921c3f2cdbea58eb3c6012458fe1c83f6
e <809299597@qq.com>
2019-11-14 17:20:10 +0800

    员工列表离职隐藏删除和重置密码按钮

--- src/view/va/staff/staffList.vue
+++ src/view/va/staff/staffList.vue
@@ -58,8 +58,8 @@
                 <el-table-column label="操作" width="240" align="center">
                     <template slot-scope="scope">
                         <el-button size="mini" @click="handle1(scope.$index, scope.row)">查看</el-button>
-                        <el-button size="mini" type="danger" @click="dodelete(scope.$index, scope.row)">删除</el-button>
-                        <el-button size="mini" type="danger" @click="handle2(scope.$index, scope.row)">重置密码</el-button>
+                        <el-button size="mini" v-show="scope.row.status==0" type="danger" @click="dodelete(scope.$index, scope.row)">删除</el-button>
+                        <el-button size="mini" v-show="scope.row.status==0" type="danger" @click="handle2(scope.$index, scope.row)">重置密码</el-button>
                     </template>
                 </el-table-column>
             </el-table>
